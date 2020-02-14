a1dcd3e6b56d7f891b91de3385219ee8fb882932
Wayne Do <dothnguyen@gmail.com>
2020-02-14 16:39:49 +1000

    zoom location dropdown - wip

--- src/app/experiment/map/map.component.ts
+++ src/app/experiment/map/map.component.ts
@@ -14,9 +14,31 @@ export class MapComponent implements OnInit, AfterViewInit {
   mapId: string;
   map: any;
 
-  @Input('defaultLocation') defaultLocation: ZoomLocation;
+  private _defaultLocation:ZoomLocation;
+  get defaultLocation() {
+    return this._defaultLocation;
+  }
 
-  @Input('zoomLocations') zoomLocations: ZoomLocation[];
+  @Input('defaultLocation')
+  set defaultLocation(val: ZoomLocation) {
+    this._defaultLocation = val;
+    // regenerate the default location
+    this.changeDefaultLocation();
+  }
+
+  private _zoomLocations: ZoomLocation[];
+  get zoomLocations() {
+    return this._zoomLocations;
+  }
+
+  @Input('zoomLocations')
+  set zoomLocations(val: ZoomLocation[]) {
+    this._zoomLocations = val;
+
+    this.buidLocationDropdown();
+  }
+
+  zoomDropdownContainer: L.Control;
 
   constructor(element: ElementRef) {
       console.log(element.nativeElement);
@@ -28,7 +50,6 @@ export class MapComponent implements OnInit, AfterViewInit {
   }
 
   ngOnInit() {
-
   }
 
   ngAfterViewInit() {
@@ -36,11 +57,11 @@ export class MapComponent implements OnInit, AfterViewInit {
   }
 
   initMap() {
-    var center = [ 39.8282, -98.5795 ];
+    var center = new L.LatLng(39.8282, -98.5795);
     var zoomLevel = 3;
-    if (this.defaultLocation) {
-      center = [this.defaultLocation.coord.lat, this.defaultLocation.coord.lng];
-      zoomLevel = this.defaultLocation.zoomLevel;
+    if (this._defaultLocation) {
+      center = new L.LatLng(this._defaultLocation.coord.lat, this._defaultLocation.coord.lng);
+      zoomLevel = this._defaultLocation.zoomLevel;
     }
 
     this.map = L.map(this.mapId, {
@@ -54,6 +75,52 @@ export class MapComponent implements OnInit, AfterViewInit {
     });
 
     tiles.addTo(this.map);
+
+    if (this._zoomLocations) {
+      this.buidLocationDropdown();
+    }
+  }
+
+  private changeDefaultLocation() {
+    if (this.map && this._defaultLocation) {
+      this.map.setView([this._defaultLocation.coord.lat, this._defaultLocation.coord.lng],
+                        this._defaultLocation.zoomLevel);
+    }
+  }
+
+  private buidLocationDropdown() {
+    if (!this.map)
+      return;
+
+    if (this.zoomDropdownContainer) {
+      this.zoomDropdownContainer.remove();
+    }
+
+    if (!this.zoomLocations || this.zoomLocations.length < 0) {
+      this.zoomDropdownContainer = new L.Control({position: 'bottomright'});
+
+      this.zoomDropdownContainer.onAdd = (map: L.Map): HTMLElement => {
+          var div = L.DomUtil.create('div', 'info legend');
+
+          var dropDownContent = '<select id="ZoomLocationDropdown" class="form-control">';
+          if (this._defaultLocation)
+            dropDownContent += `<option data-zoom="${this._defaultLocation.zoomLevel}" data-long="${this._defaultLocation.coord.lng}" data-lat="' + this._defaultLocation.coord.lat + '">${this._defaultLocation.name}</option>`;
+          this._zoomLocations.forEach(zoom => {
+            dropDownContent += `<option data-zoom="${zoom.zoomLevel}" data-long="${zoom.coord.lng}" data-lat="${zoom.coord}">${zoom.name}</option>`;
+          });
+
+          dropDownContent += '</select>';
+          div.innerHTML = dropDownContent;
+
+          //div.firstChild.onmousedown = div.firstChild.ondblclick = L.DomEvent.stopPropagation;
+          //div.firstChild.
+
+          return div;
+      }
+
+      this.zoomDropdownContainer.addTo(this.map);
+    }
+
   }
 
 }
--- src/app/pages/welcome/welcome.component.ts
+++ src/app/pages/welcome/welcome.component.ts
@@ -9,15 +9,47 @@ import { ZoomLocation } from '../../core/models/map.model';
 })
 export class WelcomeComponent implements OnInit {
 
-  zoomLocation: ZoomLocation = {
+  defaultLocation: ZoomLocation = {
     name: 'default',
-    coord: { lat: 10.7546658, lng: 106.4143583 },
+    coord: { lat: -27.4626137, lng: 153.0211694 },
     zoomLevel: 7
   };
+
+  zoomLocations: ZoomLocation[]= [
+    {
+      name: 'Gold Coast	',
+      coord: { lat: -28.0781502, lng: 153.411162 },
+      zoomLevel: 14
+    },
+    {
+      name: 'Darwin City',
+      coord: { lat: -12.4644799, lng: 130.8433367 },
+      zoomLevel: 14
+    },
+    {
+      name: 'Townsville	',
+      coord: { lat: -19.259, lng: 146.8169 },
+      zoomLevel: 14
+    },
+    {
+      name: 'Airlie Beach	',
+      coord: { lat: -20.2779275, lng: 148.691019399999 },
+      zoomLevel: 14
+    }
+  ];
+
+
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  changeLocation() {
+    this.defaultLocation = {
+      name: 'Townsville	',
+      coord: { lat: -20.2779275, lng: 148.691019399999 },
+      zoomLevel: 14
+    };
+  }
 }
