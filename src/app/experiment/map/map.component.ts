a86bdab9a006ecfce48fe281aeaab5b20a633f8a
dothnguyen <dothnguyen@gmail.com>
2020-02-16 22:43:18 +1000

    add zoom locations

--- src/app/experiment/map/map.component.ts
+++ src/app/experiment/map/map.component.ts
@@ -1,20 +1,26 @@
-import { Component, OnInit, AfterViewInit, ElementRef, Input } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  AfterViewInit,
+  ElementRef,
+  Input
+} from '@angular/core';
 
 import { ZoomLocation } from '../../core/models/map.model';
 
 import * as L from 'leaflet';
 
 @Component({
-  selector: '[map-component]',
+  // tslint:disable-next-line: component-selector
+  selector: 'map-component, [map-component]',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit, AfterViewInit {
-
   mapId: string;
   map: any;
 
-  private _defaultLocation:ZoomLocation;
+  private _defaultLocation: ZoomLocation;
   get defaultLocation() {
     return this._defaultLocation;
   }
@@ -41,38 +47,44 @@ export class MapComponent implements OnInit, AfterViewInit {
   zoomDropdownContainer: L.Control;
 
   constructor(element: ElementRef) {
-      console.log(element.nativeElement);
-      if (!element.nativeElement.id) {
-        element.nativeElement.id =  new Date().getTime().toString();
-      }
+    console.log(element.nativeElement);
+    if (!element.nativeElement.id) {
+      element.nativeElement.id = new Date().getTime().toString();
+    }
 
-      this.mapId = element.nativeElement.id;
+    this.mapId = element.nativeElement.id;
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
   ngAfterViewInit() {
     this.initMap();
   }
 
   initMap() {
-    var center = new L.LatLng(39.8282, -98.5795);
-    var zoomLevel = 3;
+    let center = new L.LatLng(39.8282, -98.5795);
+    let zoomLevel = 3;
     if (this._defaultLocation) {
-      center = new L.LatLng(this._defaultLocation.coord.lat, this._defaultLocation.coord.lng);
+      center = new L.LatLng(
+        this._defaultLocation.coord.lat,
+        this._defaultLocation.coord.lng
+      );
       zoomLevel = this._defaultLocation.zoomLevel;
     }
 
     this.map = L.map(this.mapId, {
-      center: center,
+      center,
       zoom: zoomLevel
     });
 
-    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    });
+    const tiles = L.tileLayer(
+      'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      {
+        maxZoom: 19,
+        attribution:
+          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      }
+    );
 
     tiles.addTo(this.map);
 
@@ -83,44 +95,57 @@ export class MapComponent implements OnInit, AfterViewInit {
 
   private changeDefaultLocation() {
     if (this.map && this._defaultLocation) {
-      this.map.setView([this._defaultLocation.coord.lat, this._defaultLocation.coord.lng],
-                        this._defaultLocation.zoomLevel);
+      this.map.setView(
+        [this._defaultLocation.coord.lat, this._defaultLocation.coord.lng],
+        this._defaultLocation.zoomLevel
+      );
     }
   }
 
   private buidLocationDropdown() {
-    if (!this.map)
+    if (!this.map) {
       return;
+    }
 
     if (this.zoomDropdownContainer) {
       this.zoomDropdownContainer.remove();
     }
 
-    if (!this.zoomLocations || this.zoomLocations.length < 0) {
-      this.zoomDropdownContainer = new L.Control({position: 'bottomright'});
+    if (this.zoomLocations && this.zoomLocations.length > 0) {
+      this.zoomDropdownContainer = new L.Control({ position: 'bottomright' });
 
       this.zoomDropdownContainer.onAdd = (map: L.Map): HTMLElement => {
-          var div = L.DomUtil.create('div', 'info legend');
+        const div = L.DomUtil.create('div', 'zoomto-outer');
+        div.insertAdjacentHTML('beforeend', '<span>Zoom To</span>');
+        let dropDownContent = '<select id="ZoomLocationDropdown">';
 
-          var dropDownContent = '<select id="ZoomLocationDropdown" class="form-control">';
-          if (this._defaultLocation)
-            dropDownContent += `<option data-zoom="${this._defaultLocation.zoomLevel}" data-long="${this._defaultLocation.coord.lng}" data-lat="' + this._defaultLocation.coord.lat + '">${this._defaultLocation.name}</option>`;
-          this._zoomLocations.forEach(zoom => {
-            dropDownContent += `<option data-zoom="${zoom.zoomLevel}" data-long="${zoom.coord.lng}" data-lat="${zoom.coord}">${zoom.name}</option>`;
+        if (this._defaultLocation) {
+          dropDownContent += `<option data-zoom="${this._defaultLocation.zoomLevel}"
+                              data-long="${this._defaultLocation.coord.lng}"
+                              data-lat="${this._defaultLocation.coord.lat}">${this._defaultLocation.name}</option>`;
+        }
+
+        this._zoomLocations.forEach(zoom => {
+          dropDownContent += `<option data-zoom="${zoom.zoomLevel}" data-long="${zoom.coord.lng}"
+                              data-lat="${zoom.coord.lat}">${zoom.name}</option>`;
+        });
+
+        dropDownContent += '</select>';
+        div.insertAdjacentHTML('beforeend', dropDownContent);
+
+        div
+          .querySelector('#ZoomLocationDropdown')
+          .addEventListener('change', e => {
+            console.log(e);
           });
 
-          dropDownContent += '</select>';
-          div.innerHTML = dropDownContent;
+        div.addEventListener('mousedown', L.DomEvent.stopPropagation);
+        div.addEventListener('dblclick', L.DomEvent.stopPropagation);
 
-          //div.firstChild.onmousedown = div.firstChild.ondblclick = L.DomEvent.stopPropagation;
-          //div.firstChild.
-
-          return div;
-      }
+        return div;
+      };
 
       this.zoomDropdownContainer.addTo(this.map);
     }
-
   }
-
 }
